a34a69b7ac510fbb842a9948311a0a3b463ed638
Abhiroop Santra <abhiroop.santra@gmail.com>
2023-11-22 18:38:35 +0530

    feat: Add back button, favorite button, and share button to PlaceDetails

--- components/landing/PlaceDetails.tsx
+++ components/landing/PlaceDetails.tsx
@@ -1,17 +1,42 @@
 import React from 'react';
-import { Dimensions, View, StyleSheet, Image, ScrollView } from 'react-native';
+import { Dimensions, View, StyleSheet, Image, ScrollView, TouchableOpacity } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { theme } from '../../constants/theme';
 import { Place } from '../../data';
 import AppText from '../shared/AppText';
+import { Icon } from '@rneui/themed';
+import { useNavigation } from '@react-navigation/native';
 
 const deviceWidth = Dimensions.get('window').width;
 const deviceHeight = Dimensions.get('window').height;
 const PlaceDetails = ({ place }: { place: Place }) => {
+  const navigation = useNavigation();
+
+  const goBack = () => {
+    navigation.goBack();
+  };
+
   return (
     <SafeAreaView style={styles.rootContainer}>
-      {/* <AppHeader title={place.name} /> */}
-      <Image source={place.imageUrl} style={styles.image} resizeMode="cover" />
+      <View>
+        <TouchableOpacity style={styles.goBackButton} onPress={goBack}>
+          <Icon name={'chevron-back-outline'} type="ionicon" size={20} containerStyle={styles.goBackIcon} />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.favoriteButton}>
+          <Icon
+            name={place.isFavorite ? 'bookmark' : 'bookmark-outline'}
+            type="ionicon"
+            size={20}
+            containerStyle={styles.favoriteIcon}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.shareButton}>
+          <Icon name={'share-outline'} type="ionicon" size={20} containerStyle={styles.shareIcon} />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.imageContainer}>
+        <Image source={place.imageUrl} style={styles.image} resizeMode="cover" />
+      </View>
       <ScrollView contentContainerStyle={styles.scrollContainer}>
         <View style={styles.detailsContainer}>
           <AppText style={styles.placeName}>{place.name}</AppText>
@@ -27,10 +52,13 @@ const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
     backgroundColor: theme.colors.bg100,
-    height: deviceHeight * 0.85,
+    height: deviceHeight,
   },
   scrollContainer: {
     flexGrow: 1,
+  },
+  imageContainer: {
+    zIndex: -1,
   },
   image: {
     height: 300, // Adjust the height as needed
@@ -55,6 +83,43 @@ const styles = StyleSheet.create({
   placeDescription: {
     fontSize: 16,
     color: theme.colors.text,
+  },
+  favoriteButton: {
+    zIndex: 2,
+  },
+  favoriteIcon: {
+    position: 'absolute',
+    top: deviceHeight * 0.03,
+    zIndex: 2,
+    right: deviceWidth * 0.15,
+    backgroundColor: theme.colors.white,
+    color: theme.colors.text,
+    borderRadius: 999,
+    padding: 5,
+  },
+  shareButton: {
+    zIndex: 2,
+  },
+  shareIcon: {
+    position: 'absolute',
+    top: deviceHeight * 0.03,
+    right: deviceWidth * 0.05,
+    backgroundColor: theme.colors.white,
+    color: theme.colors.text,
+    borderRadius: 999,
+    padding: 5,
+  },
+  goBackButton: {
+    zIndex: 2,
+  },
+  goBackIcon: {
+    position: 'absolute',
+    top: deviceHeight * 0.03,
+    left: deviceWidth * 0.05,
+    backgroundColor: theme.colors.white,
+    color: theme.colors.text,
+    borderRadius: 999,
+    padding: 5,
   },
 });
 
